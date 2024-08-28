d7574c101416f8b13542e07131ec05484739ce80
alanPC <alann.maman@gmail.com>
2024-08-27 22:02:48 -0300

    configured googlecreds

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,15 +1,23 @@
+import google.auth
 import os
 import json
 import vertexai
 from google.oauth2 import service_account
 from vertexai.generative_models import GenerativeModel
+import base64
 
 from dotenv import load_dotenv
 load_dotenv('config.env')
 
 
+def decode_creds():
+    credentials = json.loads(base64.b64decode(os.environ["GOOGLE_APPLICATION_CREDENTIALS"]).decode("utf-8"))
+    return credentials
+
+
 def lambda_handler(event, context):
-    credentials = service_account.Credentials.from_service_account_info(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+    credentials = service_account.Credentials.from_service_account_info(decode_creds(),
+                                                                        scopes=["https://www.googleapis.com/auth/cloud-platform"])
 
     project_id = os.environ["PROJECT_ID"]
 
@@ -29,7 +37,7 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
-    with open('../test/events/test_event.json', 'r') as file:
+    with open('test/events/test_event.json', 'r') as file:
         event = json.load(file)
     r = lambda_handler(event, None)
     print(r)
