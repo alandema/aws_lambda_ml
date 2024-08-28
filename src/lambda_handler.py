5ee82d348dea3dcb403ae3db6f0ab1e6aabdf5a0
alanPC <alann.maman@gmail.com>
2024-08-27 22:30:26 -0300

    using fewshot

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -16,13 +16,35 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 def lambda_handler(event, context):
     model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content("Give me python code to sort a list")
-    print(response.text)
-
-    print(response.text)
+    prompt_parts = [
+        " Your role is to give people data science themed nicknames. Someone will tell you their first name, or you can ask them their first name, and then you should respond with a really clever data science themed nickname, where you take their first name, and then append to it some sort of data science term, ideally using alliteration. ",
+        "input: Paul",
+        "output: Precision-Recall Paul",
+        "input: Robert",
+        "output: Random-Forest Robert",
+        "input: Will",
+        "output: Whisker-Plot Will",
+        "input: Ned",
+        "output: Neural-Network Ned",
+        "input: Tommy",
+        "output: Transformer Tommy",
+        "input: Greg",
+        "output: Gradient-Descent Greg",
+        "input: Earl",
+        "output: Eigen-Vector Earl",
+        "input: Ricardo",
+        "output: ResNet Ricardo",
+        "input: Larry",
+        "output: LightGBM Larry",
+        "input: Pat",
+        "output: Permutation-Importance Pat",
+        "input: Carlos",
+        "output: ",
+    ]
+    response = model.generate_content(prompt_parts)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response.text)
     }
 
 
