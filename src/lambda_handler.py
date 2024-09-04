690a83523c72dc47d9bd4174409b62e32ee70135
alanPC <alann.maman@gmail.com>
2024-09-03 21:18:13 -0300

    force deploy

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -13,7 +13,6 @@ SAFETY_SETTINGS = {
     HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
     HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
     HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
-
 }
 
 
